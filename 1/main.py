a1572fc1f4ce0f5eaea8828c94f2c11ceb9a8f31
prabhanshu <mail.prabhanshu@gmail.com>
2020-04-18 06:20:51 +0530

    completed

--- 1/main.py
+++ 1/main.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import timeit
 
+#pure python
+def f(n):
+    if n%3 == 0 or n%5 == 0:
+        return n
+
+multiples = [i for i in range(1,1000) if f(i)]
+summ = sum(multiples)
+
+#numpy
+x = np.arange(1,1000)
+mul = np.ma.masked_where(
+        np.logical_and((x%3 != 0),(x%5 != 0)),
+        x)
+summed = mul.sum()

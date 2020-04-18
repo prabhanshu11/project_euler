cda6c48ba9ba6e8c4c674f073044a6b82867c74c
prabhanshu <mail.prabhanshu@gmail.com>
2020-04-18 07:47:54 +0530

    third

---
+++ 3/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import math
+
+n = 13195
+x = np.arange(1,np.sqrt(n)+1)
+divisible = n%x
+#print(divisible)
+
+#print(np.ma.masked_where(n%x!=0,x))
+
+
+nums = np.array([17, 18, 19, 20, 21, 22, 23])
+divisors = np.arange(2,int(np.sqrt(np.max(nums)))+1)
+print(nums[None,:],
+      nums[:,None]% divisors[None,:]
+      )
